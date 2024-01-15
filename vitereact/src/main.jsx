d506e8c89d1b22f745151e37963ac212dcb48d0d
Deep kar <devopdeep004.com>
2024-01-15 10:58:25 +0530

    Learning react by building a tictac toe game

--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <h1>Custom react app</h1>
+  )
+}
+
+const anotherElement = (
+  <a href="https://www.google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement("a", {
+  href: "https://www.google.com",
+  target: "_blank"
+}, "Click me to visit google");
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <MyApp />
+  anotherElement
 )
