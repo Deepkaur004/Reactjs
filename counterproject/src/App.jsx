7c8940589dbe8352ec1176fb0916053f7835e8d1
Deep kar <devopdeep004.com>
2024-01-15 12:32:57 +0530

    created counter project by usestate hook

---
+++ counterproject/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+  function addValue() {
+    counter = counter + 1
+    setCounter(counter);
+  }
+
+  function removeValue() {
+    counter = counter - 1
+    if(counter < 0 ){
+      counter = 0;
+      setCounter(0);
+    }
+    setCounter(counter);
+  }
+
+  return (
+    <>
+      <h1>Counter project start</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button onClick={addValue}>Increase Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Decrease Value {counter}</button>
+    </>
+  )
+}
+
+export default App
